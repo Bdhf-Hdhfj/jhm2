c4e2cea05f44dfcfcfdbe80f9296db366e28f9f1
Bdhf-Hdhfj <140914032+Bdhf-Hdhfj@users.noreply.github.com>
2024-11-30 02:44:41 +0000

    Update

--- project1/test.py
+++ project1/test.py
@@ -19,6 +19,18 @@ print(df['Name'])
 
 
 print("\nAccess the second row using iloc:")
+print(df.iloc[1])
+
+
+df['Salary'] = [70000, 80000, 75000]
+print("\n添加 'Salary' 列后的 DataFrame:")
 print(df)
 
 
+filtered_df = df[df['Age'] > 28]
+print("\n年龄大于 28 的过滤后 DataFrame:")
+print(filtered_df)
+
+
+average_salary = df['Salary'].mean()
+print(f"\n平均工资: {average_salary}")
