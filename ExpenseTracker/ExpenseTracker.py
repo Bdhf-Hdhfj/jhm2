8fef30bdf11ca2c71c249c29a9a2f308417c5bbb
Bdhf-Hdhfj <140914032+Bdhf-Hdhfj@users.noreply.github.com>
2024-11-30 05:01:22 +0000

    Update

--- ExpenseTracker/ExpenseTracker.py
+++ ExpenseTracker/ExpenseTracker.py
@@ -1,1 +1,7 @@
-print("Hellow World")
+import pandas as pd
+
+Data = {
+    'Mumber': ['1', '2', '3', '4', '5'],  # 鍵 'Name' 對應的值是一個列表
+    'Age': [25, 30, 35],                  # 鍵 'Age' 對應的值是一個列表
+    'City': ['New York', 'Los Angeles', 'Chicago']  # 鍵 'City' 對應的值是一個列表
+}
